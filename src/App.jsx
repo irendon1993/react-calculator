6654cc60ce2f2d79f876c7ebf7c008e052d3b77c
Isaac Rendon <isaacrendon7@gmail.com>
2020-09-30 09:14:37 -0400

    Started PEDAC

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
 import React from 'react'
+import { Calculator } from './components/Calculator'
 
+// PROBLEM
+//
+// Practice using functional components with hooks
+// Practice implementing a UI wire-frame
+//
+// Requirements
+// Create a simple calculator that allows the user to add, multiply, subtract, and divide two numbers.
+//
+// EXAMPLE
+// 2 + 2 = 4
+//
+// DATA
+// 1st number to apply operation on
+// 2nd number to apply operation on
+// Total
+// Individual calculator buttons
+// Click handler
+// Ability to handle more than two numbers
+//
+//
+//
+//
 function App() {
-  return <div>Hello, World!</div>
+  return <Calculator />
 }
 
 export default App
