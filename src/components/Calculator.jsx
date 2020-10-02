2f6829ec6a7ce8c36834a7719a26f7aafd713d64
Isaac Rendon <isaacrendon7@gmail.com>
2020-10-02 12:23:28 -0400

    Fisnihed project

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,14 +1,60 @@
 import React, { useState } from 'react'
 
 export function Calculator() {
-  const [display, setDisplay] = useState(42)
+  const [display, setDisplay] = useState(0)
 
-  function clearDisplay(event) {
+  // initiate as an empty property that demands input
+  const [firstNumber, setFirstNumber] = useState('')
+  const [secondNumber, setSecondNumber] = useState('')
+  const [operator, setOperator] = useState(null)
+  // const [result, setResult] = useState(null)
+
+  function clickedClearButton(event) {
+    setFirstNumber('')
+    setSecondNumber('')
+    setOperator(null)
     setDisplay(0)
   }
 
   function clickedDigit(digit) {
-    setDisplay(digit)
+    if (operator === null) {
+      if (firstNumber.includes('.') && digit === '.') {
+        alert('nope')
+      } else {
+        setFirstNumber(`${firstNumber}${digit}`)
+        setDisplay(`${firstNumber}${digit}`)
+      }
+    } else {
+      if (secondNumber.includes('.') && digit === '.') {
+        alert('nope')
+      } else {
+        setSecondNumber(`${secondNumber}${digit}`)
+        setDisplay(`${secondNumber}${digit}`)
+      }
+    }
+  }
+
+  function clickedOperator(selectedOperator) {
+    setOperator(selectedOperator)
+  }
+
+  function clickedEquals() {
+    let result
+
+    if (operator === '+') {
+      result = Number(firstNumber) + Number(secondNumber)
+    }
+    if (operator === '-') {
+      result = Number(firstNumber) - Number(secondNumber)
+    }
+    if (operator === '*') {
+      result = Number(firstNumber) * Number(secondNumber)
+    }
+    if (operator === '/') {
+      result = Number(firstNumber) / Number(secondNumber)
+    }
+
+    setDisplay(result)
   }
 
   return (
@@ -16,13 +62,25 @@ export function Calculator() {
       <div className="calculator">
         <div className="display">{display}</div>
         <div className="buttons">
-          <button className="button fn" onClick={clearDisplay}>
+          <button className="button fn" onClick={clickedClearButton}>
             AC
           </button>
           <button className="button fn">&#177;</button>
           <button className="button fn">&#37;</button>
-          <button className="button op">&#247;</button>
-          <button className="button" onClick={() => clickedDigit(7)}>
+          <button
+            className="button op"
+            onClick={function () {
+              clickedOperator('/')
+            }}
+          >
+            &#247;
+          </button>
+          <button
+            className="button"
+            onClick={function () {
+              clickedDigit(7)
+            }}
+          >
             7
           </button>
           <button className="button" onClick={() => clickedDigit(8)}>
@@ -31,7 +89,14 @@ export function Calculator() {
           <button className="button" onClick={() => clickedDigit(9)}>
             9
           </button>
-          <button className="button op">&#215;</button>
+          <button
+            className="button op"
+            onClick={function () {
+              clickedOperator('*')
+            }}
+          >
+            &#215;
+          </button>
           <button className="button" onClick={() => clickedDigit(4)}>
             4
           </button>
@@ -42,7 +107,14 @@ export function Calculator() {
             6
           </button>
 
-          <button className="button op">&#8722;</button>
+          <button
+            className="button op"
+            onClick={function () {
+              clickedOperator('-')
+            }}
+          >
+            &#8722;
+          </button>
           <button className="button" onClick={() => clickedDigit(1)}>
             1
           </button>
@@ -52,12 +124,28 @@ export function Calculator() {
           <button className="button" onClick={() => clickedDigit(3)}>
             3
           </button>
-          <button className="button op">&#43;</button>
+          <button
+            className="button op"
+            onClick={function () {
+              clickedOperator('+')
+            }}
+          >
+            &#43;
+          </button>
           <button className="button x2" onClick={() => clickedDigit(0)}>
             0
           </button>
-          <button className="button">.</button>
-          <button className="button op">&#61;</button>
+          <button className="button" onClick={() => clickedDigit('.')}>
+            .
+          </button>
+          <button
+            className="button op"
+            onClick={() => {
+              clickedEquals()
+            }}
+          >
+            &#61;
+          </button>
         </div>
       </div>
     </main>
